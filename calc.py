e36473d161be59449870238947a15360010d6480
0xdave | Web3 Developer. <166164811+0xdave1@users.noreply.github.com>
2025-08-19 07:27:28 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,7 +10,7 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
-# Prompt the user for input
+# collect input form users
 try:
     original_price = float(input("Enter the original price of the item: "))
     discount = float(input("Enter the discount percentage: "))
